7922cbfc6bafe4878327b859d85a46fbd5a1b8ce
Jake Bloom <jake@jakebloom.me>
2024-07-05 22:27:54 -0700

    Blurb generation is working

--- src/app/result/[data]/generate.ts
+++ src/app/result/[data]/generate.ts
@@ -1,13 +1,54 @@
+import Groq from "groq-sdk";
+
+const client = new Groq({ apiKey: process.env.GROQ_API_KEY })
+const MODEL = "llama3-70b-8192"
+
+const SYSTEM_PROMPT = "You are an expert copywriter that produces blurbs for romance novels."
+
+function getUserPrompt(data: Record<string, string>): string {
+  delete data["cover"]
+  return `
+Write a funny, witty and engaging blurb for a romance novel with the following details:
+${JSON.stringify(data)}
+
+The blurb should be formatted into a few paragraphs. Return the blurb as a valid JSON in the following format:
+{'blurb': [paragraph1, paragraph2, ...]}
+  `;
+}
 
 interface GenerateResult {
-  blurb: string;
+  blurb: string[];
   image: string;
 }
 
 export async function generate(data: Record<string, string>): Promise<GenerateResult> {
-  await new Promise(res => setTimeout(res, 5000));
+  const groqRes = await client.chat.completions.create({
+    model: MODEL,
+    max_tokens: 512,
+    response_format: {type: 'json_object'},
+    messages: [
+      {role: "system", content: SYSTEM_PROMPT},
+      {role: "user", content: getUserPrompt(data)},
+    ],
+  });
+
+  const content = groqRes.choices[0].message.content
+
   return {
-    blurb: "",
+    blurb: JSON.parse(content || "{}").blurb || "",
     image: "",
+  }
+}
+
+const MOCK_BLURB = [
+  "In the small town of Oakdale, where everyone knows everyone and gossip spreads like wildfire, Dr. Sophia Thompson has made a name for herself as the most cynical and unromantic doctor in the county. She's got a reputation for being immune to the charms of the local ladies' men, and her no-nonsense attitude has earned her a few enemies along the way.",
+  "One of those enemies is none other than Ryder Flynn, the star quarterback of the Oakdale Falcons, who's as chiseled as he is charming. Their families have been at odds for decades, and Sophia and Ryder have grown up on opposite sides of a bitter family feud. The last thing they need is to be forced together, but fate has other plans.",
+  "When Sophia's medical expertise is required to treat Ryder's injured shoulder, their constant bickering and sarcastic banter can't hide the undeniable spark between them. As they work together to uncover the secrets of their families' long-standing feud, Sophia's icy exterior begins to thaw, and Ryder's protective instincts go into overdrive. Can these sworn enemies turn their animosity into something more? Or will their families' past tear them apart once and for all?",
+]
+
+export async function generateMock(data: any): Promise<GenerateResult> {
+  return {
+    blurb: MOCK_BLURB,
+    image: "https://loremflickr.com/1024/1024",
   }
 }
--- src/app/result/[data]/page.tsx
+++ src/app/result/[data]/page.tsx
@@ -1,4 +1,6 @@
+import Link from "next/link";
 import { generate } from "./generate";
+import Image from "next/image";
 
 interface Params {
   params: {
@@ -13,7 +15,26 @@ export default async function Result({params: {data}}: Params): Promise<JSX.Elem
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between py-4">
-      {JSON.stringify(result)}
+      <div className="md:w-1/2 sm:w-full sm:px-12 md:px-0 w-full flex flex-col justify-center">
+        <div className="flex justify-between items-center w-full flex-col md:flex-row mb-8">
+          <Link href="/"><h1 className="font-bold text-2xl text-blue">Romance Novel Generator</h1></Link>
+          <Link href="/about" className="text-blue">About</Link>
+        </div>
+
+        <div className="grid xl:grid-cols-2 gap-4 mb-4">
+          <div className="place-self-center">
+            <Image
+              src={result.image}
+              className="px-8 sm:px-0 rounded"
+              width={1024}
+              height={1024}
+              alt="The cover illustration for the book" />
+          </div>
+          <div className="px-8 sm:px-0 flex flex-col gap-4">
+            {result.blurb.map((a, idx) => <div key={idx}>{a}</div>)}
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
