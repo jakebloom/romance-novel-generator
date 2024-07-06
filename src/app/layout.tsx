d560daa85b7d8312f7b7e6308a7e8eabb5ff1f2b
Jake Bloom <jake@jakebloom.me>
2024-07-05 23:45:03 -0700

    Adsense metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
-// @ts-nocheck
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Romance Novel Generator",
   description: "Create the novel of your dreams",
+  other: {
+    "google-adsense-account": "ca-pub-9245880302902366",
+  }
 };
 
 export default function RootLayout({
@@ -18,10 +19,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9245880302902366"
-          crossorigin="anonymous"></script>
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
